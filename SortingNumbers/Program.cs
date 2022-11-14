607187508dff99959257ca96baa5de0791095ae4
Yuşe Yusuf Kılıç <79745654+yuseyusufkilic@users.noreply.github.com>
2022-11-14 16:51:48 +0300

    Add project files.

---
+++ SortingNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] nums = { 11, 22, 54, 87, 32, 87, 27 };
+
+for (int i = 0; i < nums.Length; i++)
+{
+    for (int j = 0; j < nums.Length; j++)
+    {
+        int temp = 0;
+        if (nums[i] < nums[j])
+        {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+
+        }
+
+    }
+
+}
+foreach (var num in nums)
+{
+    Console.WriteLine(num);
+
+}
