bb8c82bb8db4f7cba7044d5cc514c5c8fbd59dfc
Yuşe Yusuf Kılıç <79745654+yuseyusufkilic@users.noreply.github.com>
2022-11-14 23:52:20 +0300

    A problem solved which is stack implementation with using Queue.

---
+++ ImplementStackUsingQueue/Program.cs
@@ -0,0 +1,15 @@
+﻿using ImplementStackUsingQueue;
+using System.Collections;
+
+StackUsingQueue stackUsingQueue = new StackUsingQueue();
+stackUsingQueue.Push(5);
+stackUsingQueue.Push(15);
+stackUsingQueue.Push(56);
+
+stackUsingQueue.Pop();
+
+foreach (var item in stackUsingQueue.Queue)
+{
+    Console.WriteLine(item);
+
+}
---
+++ ImplementStackUsingQueue/StackUsingQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementStackUsingQueue
+{
+    public class StackUsingQueue
+    {
+        public Queue<int> Queue { get; set; } = new Queue<int>();
+        public void Push(int x)
+        {
+            Queue.Enqueue(x);
+        }
+        public int Top()
+        {
+            return Queue.ElementAt(Queue.Count-1);
+        }
+        public void Pop()
+        {
+            var last = Queue.Last();
+            for (int i = 0; i < Queue.Count; i++)
+            {
+                var deleted = Queue.Dequeue();
+                if (deleted != last)
+                    Queue.Enqueue(deleted);
+
+            }
+        }
+        public bool isEmpty()
+        {
+            return Queue.Count() == 0;
+        }
+    }
+}
